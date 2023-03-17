5de6da304b1357ba4f68288aa67a8529100749d4
Rajesh <rbajajece@gmail.com>
2023-03-18 00:29:12 +0530

    updated code

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_security_group" "nginx" {
-  name   = "nginx_access"
+resource "aws_security_group" "rajesh" {
+  name   = "rajesh_access"
   vpc_id = local.vpc_id
 
   ingress {
@@ -40,21 +40,62 @@ resource "aws_security_group" "nginx" {
   }
 }
 
-resource "aws_key_pair" "ec2-keypair" {
-  key_name   = "authorized_keys"
-  public_key = "sh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCmIN4kybSZ3kd+rwX8QoyPui4EOMZEeFbP/BpBZ1A15cyQ1vWgQLRlyr41MKIjfFa6Nrlf3H+GiTbsAjYicpz2uXzsrOOQC+1aT7vK4RcXSjPvxtZgJw4BMwbES0j492akqtXb9fP5uEumt3rkNhmkb0olFEqeEsB1qFXbruTFbiNhyfybdkfTyZ4qQOUdm+3ZSVDJUanGTI2NqyVzNeGtQtBiEDFTP0I6sEjAckEetr8IA0LP5VF1GTWIZIxn002pcln8Xts/tYZ1r3FoMuI6BtDcKTkm3fpQzLc0NZEG/bB/BahMUhwTfO/nYA9NRjAwoHLlv23jveQ4cDjb7tGj devops-project"
-}
-
+# resource "aws_key_pair" "tf-key-pair" {
+# key_name = "tf-key-pair"
+# public_key = tls_private_key.rsa.public_key_openssh
+# }
+# resource "tls_private_key" "rsa" {
+# algorithm = "RSA"
+# rsa_bits  = 4096
+# }
+# resource "local_file" "tf-key" {
+# content  = tls_private_key.rsa.private_key_pem
+# filename = "tf-key-pair"
+# }
 resource "aws_instance" "nginx" {
   ami                         = "ami-05375ba8414409b07"
   subnet_id                   = "subnet-0c298bca95293811e"
   instance_type               = "t2.micro"
   associate_public_ip_address = true
-  security_groups             = [aws_security_group.nginx.id]
-  key_name                    = aws_key_pair.ec2-keypair.key_name
-  
-  }
+  security_groups             = [aws_security_group.rajesh.id]
+  key_name                    = "devops-project"
+ 
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt update -y
+    sudo apt install apache2 -y
+    sudo systemctl start apache2
+    sudo bash -c 'echo your very first web server > /var/www/html/index.html'
+    EOF
 
+  tags = {
+  Name = "first-ec2-server"
+}
+connection {
+type = "ssh"
+user = "ubuntu"
+private_key = file("./devops-project.pem")
+host = self.public_ip
+}
 
+provisioner "remote-exec" {
+inline = [
+    "sleep 10",
+    "sudo apt-get update",
+    "curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -",
+    "sudo apt install nodejs",
+    "git clone https://github.com/rajeshkbajaj/Express.git",
+    "cd /home/ubuntu/Express",
+    "npm install",
+    "cd ./examples/hello-world/",
+    "node index.js"
+  ]
+}
+
+}
+
+output "server_public_ip" {
+value = aws_instance.nginx.public_ip
+}
 
 
